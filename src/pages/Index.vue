4ec76873d52fa5e762291dd2c6400e6685221af6
Lucas Corrêa <lucascorrea@MacBook-Air-de-Lucas.local>
2023-08-08 20:12:26 -0300

    Alterado nome da escola

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,30 +1,38 @@
 <template>
   <q-page class="flex flex-center">
-    <div class="row justify-center" style="width: 100%;">
+    <div class="row justify-center" style="width: 100%">
       <div class="col-lg-8 col-md-10 col-xs-11 q-ma-md">
-        <q-card class="card-bg text-black ">
-          <q-form
-            @submit="submit"
-          >
+        <q-card class="card-bg text-black">
+          <q-form @submit="submit">
             <div>
               <q-card-section class="text-h6">
                 <div class="row">
                   <div class="text-h6">EMITIR CARTEIRINHA</div>
                 </div>
               </q-card-section>
-              <q-card-section class="q-pa-sm ">
+              <q-card-section class="q-pa-sm">
                 <q-list class="row justify-center">
-                  <q-item class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+                  <q-item class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                     <q-item-section>
-                      <q-input color="bg-grey-6" dense v-model="name" label="Nome" />
+                      <q-input
+                        color="bg-grey-6"
+                        dense
+                        v-model="name"
+                        label="Nome"
+                      />
                     </q-item-section>
                   </q-item>
-                  <q-item class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+                  <q-item class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                     <q-item-section>
-                      <q-input color="bg-grey-6" dense v-model="address" label="Endereço" />
+                      <q-input
+                        color="bg-grey-6"
+                        dense
+                        v-model="address"
+                        label="Endereço"
+                      />
                     </q-item-section>
                   </q-item>
-                  <q-item class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+                  <q-item class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                     <q-item-section>
                       <q-input
                         dense
@@ -34,28 +42,36 @@
                       />
                     </q-item-section>
                   </q-item>
-                  <q-item class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+                  <q-item class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                     <q-item-section>
-                      <q-input
-                        dense
-                        v-model="course"
-                        label="Curso"
-                      />
+                      <q-input dense v-model="course" label="Curso" />
                     </q-item-section>
                   </q-item>
-                  <q-item class="col-12" style="justify-content: center; display: flex;">
+                  <q-item
+                    class="col-12"
+                    style="justify-content: center; display: flex"
+                  >
                     <q-item-section>
-                      <div class="file has-name is-boxed" style="justify-content: center; display: flex;">
+                      <div
+                        class="file has-name is-boxed"
+                        style="justify-content: center; display: flex"
+                      >
                         <label class="file-label">
-                          <input class="file-input" type="file" @input="pickFile" ref="fileInput" name="resume" @click="selectImage" accept="image/png, image/jpeg">
+                          <input
+                            class="file-input"
+                            type="file"
+                            @input="pickFile"
+                            ref="fileInput"
+                            name="resume"
+                            @click="selectImage"
+                            accept="image/png, image/jpeg"
+                          />
                           <span class="file-cta">
-                            <q-icon name="upload" size="sm"/>
-                            <span class="file-label">
-                              Selecione a foto…
-                            </span>
+                            <q-icon name="upload" size="sm" />
+                            <span class="file-label"> Selecione a foto… </span>
                           </span>
                           <span class="file-name" v-if="fileName !== ''">
-                            {{fileName}}
+                            {{ fileName }}
                           </span>
                         </label>
                       </div>
@@ -64,59 +80,72 @@
                 </q-list>
               </q-card-section>
               <q-card-actions full-width>
-                <div class="col-12" style="justify-content: right; display: flex;">
-                  <q-btn class="text-capitalize q-ma-md" color="primary" flat @click="confirmClear()" style="width: 120px;" :label="'LIMPAR'"/>
-                  <q-btn class="text-capitalize bg-primary text-white q-ma-md" @click="generate()" style="width: 120px;" :label="'ENVIAR'"/>
+                <div
+                  class="col-12"
+                  style="justify-content: right; display: flex"
+                >
+                  <q-btn
+                    class="text-capitalize q-ma-md"
+                    color="primary"
+                    flat
+                    @click="confirmClear()"
+                    style="width: 120px"
+                    :label="'LIMPAR'"
+                  />
+                  <q-btn
+                    class="text-capitalize bg-primary text-white q-ma-md"
+                    @click="generate()"
+                    style="width: 120px"
+                    :label="'ENVIAR'"
+                  />
                 </div>
               </q-card-actions>
             </div>
-            </q-form>
-
-          </q-card>
-        </div>
+          </q-form>
+        </q-card>
+      </div>
     </div>
-
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
-import pdfGeneration from '../tools/pdf_generator'
-import { alert, confirmDialog } from '../tools/messages'
+import pdfGeneration from "../tools/pdf_generator";
+import { alert, confirmDialog } from "../tools/messages";
 
 export default defineComponent({
-  name: 'PageIndex',
-  data () {
+  name: "PageIndex",
+  data() {
     return {
-      base64: '',
-      name: '',
-      address: '',
-      phoneNumber: '',
-      course: '',
-      fileName: ''
-    }
+      base64: "",
+      name: "",
+      address: "",
+      phoneNumber: "",
+      course: "",
+      fileName: "",
+    };
   },
   methods: {
-    generate () {
-      if (this.name === '') {
-        alert('Informe seu nome')
+    generate() {
+      if (this.name === "") {
+        alert("Informe seu nome");
       } else if (this.name.length > 41) {
-        alert('Limite máximo de 41 caracteres no nome')
-      } else if (this.address === '') {
-        alert('Informe seu endereço')
+        alert("Limite máximo de 41 caracteres no nome");
+      } else if (this.address === "") {
+        alert("Informe seu endereço");
       } else if (this.address.length > 41) {
-        alert('Limite máximo de 41 caracteres no endereço')
-      } else if (this.phoneNumber === '') {
-        alert('Informe seu telefone')
+        alert("Limite máximo de 41 caracteres no endereço");
+      } else if (this.phoneNumber === "") {
+        alert("Informe seu telefone");
       } else if (this.phoneNumber.length > 41) {
-        alert('Limite máximo de 41 caracteres no telefone')
-      } else if (this.course === '') {
-        alert('Informe seu curso')
+        alert("Limite máximo de 41 caracteres no telefone");
+      } else if (this.course === "") {
+        alert("Informe seu curso");
       } else if (this.course.length > 41) {
-        alert('Limite máximo de 41 caracteres no curso')
-      } else if (this.base64 === '') {
-        alert('Insira uma foto')
+        alert("Limite máximo de 41 caracteres no curso");
+      } else if (this.base64 === "") {
+        alert("Insira uma foto");
       } else {
         try {
           pdfGeneration.generate(
@@ -124,45 +153,42 @@ export default defineComponent({
             this.address.toUpperCase(),
             this.phoneNumber,
             this.course.toUpperCase(),
-            this.base64
-          )
-          this.clear()
+            this.base64,
+            "ETEC"
+          );
+          this.clear();
         } catch (error) {
-          alert(error, 'Erro')
+          alert(error, "Erro");
         }
       }
     },
-    confirmClear () {
-      confirmDialog(
-        'Atenção',
-        'Deseja realmente apagar?',
-        this.clear
-      )
+    confirmClear() {
+      confirmDialog("Atenção", "Deseja realmente apagar?", this.clear);
     },
-    clear () {
-      this.name = ''
-      this.address = ''
-      this.phoneNumber = ''
-      this.course = ''
-      this.base64 = ''
-      this.fileName = ''
+    clear() {
+      this.name = "";
+      this.address = "";
+      this.phoneNumber = "";
+      this.course = "";
+      this.base64 = "";
+      this.fileName = "";
     },
 
-    pickFile () {
-      const input = this.$refs.fileInput
-      const file = input.files
-      this.fileName = file[0].name
+    pickFile() {
+      const input = this.$refs.fileInput;
+      const file = input.files;
+      this.fileName = file[0].name;
 
       if (file && file[0]) {
-        const reader = new FileReader()
+        const reader = new FileReader();
 
         reader.onload = () => {
-          this.base64 = reader.result
-        }
+          this.base64 = reader.result;
+        };
 
-        reader.readAsDataURL(file[0])
+        reader.readAsDataURL(file[0]);
       }
-    }
-  }
-})
+    },
+  },
+});
 </script>
--- src/tools/pdf_generator.js
+++ src/tools/pdf_generator.js
@@ -1,76 +1,86 @@
-import { jsPDF } from 'jspdf'
-import { returnType } from '../tools/return_type'
+import { jsPDF } from "jspdf";
+import { returnType } from "../tools/return_type";
 
 class PDFGenerator {
-  async generate (nome, endereco, telefone, curso, image) {
+  async generate(nome, endereco, telefone, curso, image, instituicao) {
     // eslint-disable-next-line new-cap
     const doc = new jsPDF({
-      orientation: 'landscape',
-      unit: 'mm'
-    })
+      orientation: "landscape",
+      unit: "mm",
+    });
 
-    const margin = 10
+    const margin = 10;
 
     // tamanho da area da carteirinha
-    doc.setDrawColor(0)
-    doc.setFillColor(255, 255, 255)
-    doc.rect(0 + margin, 0 + margin, 86, 56, 'FD')
+    doc.setDrawColor(0);
+    doc.setFillColor(255, 255, 255);
+    doc.rect(0 + margin, 0 + margin, 86, 56, "FD");
 
     // header azul
-    doc.setDrawColor(0)
-    doc.setFillColor(55, 80, 162)
-    doc.rect(1 + margin, 1 + margin, 84, 11, 'F')
+    doc.setDrawColor(0);
+    doc.setFillColor(55, 80, 162);
+    doc.rect(1 + margin, 1 + margin, 84, 11, "F");
 
     // titulo detro do header
-    doc.setTextColor(255, 255, 255)
-    doc.setFontSize(11).setFont(undefined, 'bold')
-    doc.text('Carteira de Transporte Intermunicipal', 8 + margin, 8 + margin)
+    doc.setTextColor(255, 255, 255);
+    doc.setFontSize(11).setFont(undefined, "bold");
+    doc.text("Carteira de Transporte Intermunicipal", 8 + margin, 8 + margin);
 
     // imagem
-    doc.addImage(image, returnType(image), 1 + margin, 13 + margin, 21, 26, null, 'FAST', 0)
+    doc.addImage(
+      image,
+      returnType(image),
+      1 + margin,
+      13 + margin,
+      21,
+      26,
+      null,
+      "FAST",
+      0
+    );
 
-    doc.setTextColor(0, 0, 0)
-    doc.setFontSize(7).setFont(undefined, 'normal')
-    doc.text('Nome', 24 + margin, 15 + margin)
+    doc.setTextColor(0, 0, 0);
+    doc.setFontSize(7).setFont(undefined, "normal");
+    doc.text("Nome", 24 + margin, 15 + margin);
 
-    doc.setTextColor(0, 0, 0)
-    doc.setFontSize(7).setFont(undefined, 'bold')
-    doc.text(nome, 24 + margin, 18 + margin)
+    doc.setTextColor(0, 0, 0);
+    doc.setFontSize(7).setFont(undefined, "bold");
+    doc.text(nome, 24 + margin, 18 + margin);
 
-    doc.setTextColor(0, 0, 0)
-    doc.setFontSize(7).setFont(undefined, 'normal')
-    doc.text('Endereço', 24 + margin, 22 + margin)
+    doc.setTextColor(0, 0, 0);
+    doc.setFontSize(7).setFont(undefined, "normal");
+    doc.text("Endereço", 24 + margin, 22 + margin);
 
-    doc.setTextColor(0, 0, 0)
-    doc.setFontSize(7).setFont(undefined, 'bold')
-    doc.text(endereco, 24 + margin, 25 + margin)
+    doc.setTextColor(0, 0, 0);
+    doc.setFontSize(7).setFont(undefined, "bold");
+    doc.text(endereco, 24 + margin, 25 + margin);
 
-    doc.setTextColor(0, 0, 0)
-    doc.setFontSize(7).setFont(undefined, 'normal')
-    doc.text('Telefone', 24 + margin, 29 + margin)
+    doc.setTextColor(0, 0, 0);
+    doc.setFontSize(7).setFont(undefined, "normal");
+    doc.text("Telefone", 24 + margin, 29 + margin);
 
-    doc.setTextColor(0, 0, 0)
-    doc.setFontSize(7).setFont(undefined, 'bold')
-    doc.text(telefone, 24 + margin, 32 + margin)
+    doc.setTextColor(0, 0, 0);
+    doc.setFontSize(7).setFont(undefined, "bold");
+    doc.text(telefone, 24 + margin, 32 + margin);
 
-    doc.setTextColor(0, 0, 0)
-    doc.setFontSize(7).setFont(undefined, 'normal')
-    doc.text('Curso', 24 + margin, 36 + margin)
+    doc.setTextColor(0, 0, 0);
+    doc.setFontSize(7).setFont(undefined, "normal");
+    doc.text("Curso", 24 + margin, 36 + margin);
 
-    doc.setTextColor(0, 0, 0)
-    doc.setFontSize(7).setFont(undefined, 'bold')
-    doc.text(curso, 24 + margin, 39 + margin)
+    doc.setTextColor(0, 0, 0);
+    doc.setFontSize(7).setFont(undefined, "bold");
+    doc.text(curso, 24 + margin, 39 + margin);
 
-    doc.setDrawColor(0)
-    doc.setFillColor(55, 80, 162)
-    doc.rect(1 + margin, 40 + margin, 84, 15, 'F')
+    doc.setDrawColor(0);
+    doc.setFillColor(55, 80, 162);
+    doc.rect(1 + margin, 40 + margin, 84, 15, "F");
 
-    doc.setTextColor(255, 255, 255)
-    doc.setFontSize(12).setFont(undefined, 'bold')
-    doc.text('UNIP', 39 + margin, 48 + margin)
+    doc.setTextColor(255, 255, 255);
+    doc.setFontSize(12).setFont(undefined, "bold");
+    doc.text(instituicao ?? "UNIP", 39 + margin, 48 + margin);
 
-    doc.save('Carterinha.pdf')
+    doc.save("Carterinha.pdf");
   }
 }
 
-export default new PDFGenerator()
+export default new PDFGenerator();
